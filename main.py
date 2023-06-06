483d02dfac624cb37d3025142e510ee98223c80b
kodad-nadia <kodadnadiaa@gmail.com>
2023-06-06 14:51:48 +0200

    desc

--- main.py
+++ main.py
@@ -2,5 +2,26 @@ from fastapi import FastAPI
 app = FastAPI() #variable names for the servers
 
 @app.get("/")
-async def root():
-      return{"message":"coucou"}
+
+
+class ClothingItem:
+    def __init__(self, name, price, color):
+        self.name = name
+        self.price = price
+        self.color = color
+
+# Creating clothing items
+item1 = ClothingItem("T-shirt", 20.99, "Blue")
+item2 = ClothingItem("Jeans", 49.99, "Black")
+item3 = ClothingItem("Dress", 39.99, "Red")
+
+# Creating a list of clothing items
+clothes_list = [item1, item2, item3]
+
+# Accessing and printing information about each item in the list
+for item in clothes_list:
+    print(f"Name: {item.name}")
+    print(f"Price: {item.price}")
+    print(f"Color: {item.color}")
+    print("---")
+
