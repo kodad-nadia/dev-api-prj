d400f431118f8e5dbc6f1febe33fa79a0c4d3948
kodad-nadia <kodadnadiaa@gmail.com>
2023-06-06 15:29:33 +0200

    upd

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 app = FastAPI() #variable names for the servers
 
+
+# List of clothing items
+clothes = [
+    {"name": "shirt jane", "type": "shirt", "color": "Blue", "price": 20.99},
+    {"name": "pants", "type": "trousers", "color": "Black", "price": 49.99},
+    {"name": "dress amber", "type": "dress", "color": "Red", "price": 39.99},
+    {"name": "skirt jolen", "type": "skirt", "color": "Yellow", "price": 29.99},
+]
+
 @app.get("/clothes")
 async def get_clothes():
-  
- clothes = ["shirt jane","pants","dress amber","skirt jolen"]
- return clothes
+    return {"clothes": clothes}
