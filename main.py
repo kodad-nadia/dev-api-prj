c461ae9bb3b1d18bf6922f5b60df6a3142611d40
kodad-nadia <kodadnadiaa@gmail.com>
2023-06-06 15:10:44 +0200

    2

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 app = FastAPI() #variable names for the servers
 
-@app.get("clothes")
-async def root():
-      return{"shirt jane","pants","dress amber","skirt jolen"}
-
+@app.get("/clothes")
+async def get_clothes():
+  
+ clothes = ["shirt jane","pants","dress amber","skirt jolen"]
+ return clothes
