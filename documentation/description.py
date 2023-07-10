7eb32805e5589e735e43d80320899a1ed1f75d01
kodad-nadia <nadia.kodad@cap4lab.com>
2023-07-10 16:42:58 +0200

    final project nadia

--- documentation/description.py
+++ documentation/description.py
@@ -17,8 +17,3 @@ You'll be able to signup and login
 You'll be able to list and create transactions
 """
 
-api_description = """
-Shop-Wise API is a comprehensive shopping API that allows users to browse, search, and purchase a wide range of products online. It provides a robust set of endpoints to handle various aspects of the shopping experience, including managing products, user accounts, and orders. 
-The API is designed to be intuitive, scalable, and secure, making it an ideal choice for building modern e-commerce applications.
-Shop-Wise API is a RESTful API designed for managing a shopping application. 
-"""
